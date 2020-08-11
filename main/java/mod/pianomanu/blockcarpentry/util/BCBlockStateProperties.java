ab8d91f70e4dc0da50cc91f56d9ac0ab7218c1bb
Manuel <jirsakm@web.de>
2020-08-11 18:18:39 +0200

    Removed duplicate dictionary (leftover from a broken commit)

--- main/java/mod/pianomanu/blockcarpentry/util/BCBlockStateProperties.java
+++
@@ -1,7 +0,0 @@
-package mod.pianomanu.blockcarpentry.util;
-
-import net.minecraft.state.BooleanProperty;
-
-public class BCBlockStateProperties {
-    public static final BooleanProperty CONTAINS_BLOCK = BooleanProperty.create("contains_block");
-}
