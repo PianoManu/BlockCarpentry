4183a59248cbe8be1afe82de4af944e27fcc0997
Manuel <jirsakm@web.de>
2022-02-06 21:55:03 +0100

    For some reason gradlew and gradlew.bat got corrupted...

--- gradlew.bat
+++ gradlew.bat
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
@@ -18,8 +14,6 @@
 @rem limitations under the License.
 @rem
 
->>>>>>> 243d4f3 (Started porting the mod to 1.17)
->>>>>>> 3d9c0b1 (Started porting the mod to 1.17)
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
@@ -35,36 +29,18 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-<<<<<<< HEAD
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
-=======
-<<<<<<< HEAD
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=
-=======
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
->>>>>>> 243d4f3 (Started porting the mod to 1.17)
->>>>>>> 3d9c0b1 (Started porting the mod to 1.17)
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-<<<<<<< HEAD
-if "%ERRORLEVEL%" == "0" goto init
-=======
-<<<<<<< HEAD
-if "%ERRORLEVEL%" == "0" goto init
-=======
 if "%ERRORLEVEL%" == "0" goto execute
->>>>>>> 243d4f3 (Started porting the mod to 1.17)
->>>>>>> 3d9c0b1 (Started porting the mod to 1.17)
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
@@ -78,15 +54,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-<<<<<<< HEAD
-if exist "%JAVA_EXE%" goto init
-=======
-<<<<<<< HEAD
-if exist "%JAVA_EXE%" goto init
-=======
 if exist "%JAVA_EXE%" goto execute
->>>>>>> 243d4f3 (Started porting the mod to 1.17)
->>>>>>> 3d9c0b1 (Started porting the mod to 1.17)
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
@@ -96,48 +64,14 @@ echo location of your Java installation.
 
 goto fail
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 3d9c0b1 (Started porting the mod to 1.17)
-:init
-@rem Get command-line arguments, handling Windows variants
-
-if not "%OS%" == "Windows_NT" goto win9xME_args
-
-:win9xME_args
-@rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
-set _SKIP=2
-
-:win9xME_args_slurp
-if "x%~1" == "x" goto execute
-
-set CMD_LINE_ARGS=%*
-
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 243d4f3 (Started porting the mod to 1.17)
->>>>>>> 3d9c0b1 (Started porting the mod to 1.17)
 :execute
 @rem Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-<<<<<<< HEAD
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
-=======
-<<<<<<< HEAD
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
-=======
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
->>>>>>> 243d4f3 (Started porting the mod to 1.17)
->>>>>>> 3d9c0b1 (Started porting the mod to 1.17)
 
 :end
 @rem End local scope for the variables with windows NT shell
