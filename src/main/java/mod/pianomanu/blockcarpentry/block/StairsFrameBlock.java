005c5ec6b5451bd29172f7562f83d2b95ba863c4
Manuel <jirsakm@web.de>
2022-11-14 18:12:55 +0100

    Fixed stairs and slopes to match the IFrameBlock interface

--- src/main/java/mod/pianomanu/blockcarpentry/block/StairsFrameBlock.java
+++ src/main/java/mod/pianomanu/blockcarpentry/block/StairsFrameBlock.java
@@ -1,18 +1,11 @@
 package mod.pianomanu.blockcarpentry.block;
 
-import mod.pianomanu.blockcarpentry.item.BaseFrameItem;
-import mod.pianomanu.blockcarpentry.item.BaseIllusionItem;
-import mod.pianomanu.blockcarpentry.setup.Registration;
-import mod.pianomanu.blockcarpentry.setup.config.BCModConfig;
 import mod.pianomanu.blockcarpentry.tileentity.FrameBlockTile;
 import mod.pianomanu.blockcarpentry.util.BCBlockStateProperties;
-import mod.pianomanu.blockcarpentry.util.BlockAppearanceHelper;
-import mod.pianomanu.blockcarpentry.util.BlockSavingHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
@@ -33,26 +26,25 @@ import net.minecraft.world.phys.BlockHitResult;
 import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
-import static mod.pianomanu.blockcarpentry.util.BCBlockStateProperties.LIGHT_LEVEL;
-
 /**
  * Main class for frame stairs - all important block info can be found here
  * Visit {@link FrameBlock} for a better documentation
  *
  * @author PianoManu
- * @version 1.2 11/07/22
+ * @version 1.3 11/14/22
  */
-public class StairsFrameBlock extends StairBlock implements EntityBlock {
+public class StairsFrameBlock extends StairBlock implements EntityBlock, IFrameBlock {
 
     public static final BooleanProperty CONTAINS_BLOCK = BCBlockStateProperties.CONTAINS_BLOCK;
 
     public StairsFrameBlock(Supplier<BlockState> state, BlockBehaviour.Properties properties) {
         super(state, properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(CONTAINS_BLOCK, Boolean.FALSE).setValue(FACING, Direction.NORTH).setValue(HALF, Half.BOTTOM).setValue(SHAPE, StairsShape.STRAIGHT).setValue(WATERLOGGED, Boolean.valueOf(false)).setValue(LIGHT_LEVEL, 0));
+        this.registerDefaultState(this.stateDefinition.any().setValue(CONTAINS_BLOCK, Boolean.FALSE).setValue(StairBlock.FACING, Direction.NORTH).setValue(HALF, Half.BOTTOM).setValue(SHAPE, StairsShape.STRAIGHT).setValue(StairBlock.WATERLOGGED, Boolean.valueOf(false)).setValue(LIGHT_LEVEL, 0));
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(CONTAINS_BLOCK, FACING, HALF, SHAPE, WATERLOGGED, LIGHT_LEVEL);
+        super.createBlockStateDefinition(builder);
+        builder.add(CONTAINS_BLOCK, LIGHT_LEVEL);
     }
 
     @Nullable
@@ -64,78 +56,10 @@ public class StairsFrameBlock extends StairBlock implements EntityBlock {
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitresult) {
         ItemStack item = player.getItemInHand(hand);
-        if (!level.isClientSide) {
-            if (removeBlock(level, pos, state, item, player))
-                return InteractionResult.SUCCESS;
-            if (BlockAppearanceHelper.setLightLevel(item, state, level, pos, player, hand) ||
-                    BlockAppearanceHelper.setTexture(item, state, level, player, pos) ||
-                    BlockAppearanceHelper.setDesign(level, pos, player, item) ||
-                    BlockAppearanceHelper.setDesignTexture(level, pos, player, item) ||
-                    BlockAppearanceHelper.setOverlay(level, pos, player, item) ||
-                    BlockAppearanceHelper.setRotation(level, pos, player, item))
-                return InteractionResult.SUCCESS;
-            if (item.getItem() instanceof BlockItem) {
-                if (state.getValue(BCBlockStateProperties.CONTAINS_BLOCK) || item.getItem() instanceof BaseFrameItem || item.getItem() instanceof BaseIllusionItem) {
-                    return InteractionResult.PASS;
-                }
-                BlockEntity tileEntity = level.getBlockEntity(pos);
-                int count = player.getItemInHand(hand).getCount();
-                if (tileEntity instanceof FrameBlockTile && !item.isEmpty() && BlockSavingHelper.isValidBlock(((BlockItem) item.getItem()).getBlock()) && !state.getValue(CONTAINS_BLOCK)) {
-                    ((FrameBlockTile) tileEntity).clear();
-                    BlockState handBlockState = ((BlockItem) item.getItem()).getBlock().defaultBlockState();
-                    ((FrameBlockTile) tileEntity).setMimic(handBlockState);
-                    insertBlock(level, pos, state, handBlockState);
-                    if (!player.isCreative())
-                        player.getItemInHand(hand).setCount(count - 1);
-
-                }
-            }
+        if (!level.isClientSide && hand == InteractionHand.MAIN_HAND) {
+            return frameUse(state, level, pos, player, hand, hitresult);
         }
         return item.getItem() instanceof BlockItem ? InteractionResult.SUCCESS : InteractionResult.PASS;
-    }
-
-    private boolean removeBlock(Level level, BlockPos pos, BlockState state, ItemStack itemStack, Player player) {
-        if (itemStack.getItem() == Registration.HAMMER.get() || (!BCModConfig.HAMMER_NEEDED.get() && player.isCrouching())) {
-            if (!player.isCreative())
-                this.dropContainedBlock(level, pos);
-            state = state.setValue(CONTAINS_BLOCK, Boolean.FALSE);
-            level.setBlock(pos, state, 2);
-            return true;
-        }
-        return false;
-    }
-
-    private void dropContainedBlock(Level levelIn, BlockPos pos) {
-        if (!levelIn.isClientSide) {
-            BlockEntity tileentity = levelIn.getBlockEntity(pos);
-            if (tileentity instanceof FrameBlockTile) {
-                FrameBlockTile frameBlockEntity = (FrameBlockTile) tileentity;
-                BlockState blockState = frameBlockEntity.getMimic();
-                if (!(blockState == null)) {
-                    levelIn.levelEvent(1010, pos, 0);
-                    frameBlockEntity.clear();
-                    float f = 0.7F;
-                    double d0 = (double) (levelIn.random.nextFloat() * 0.7F) + (double) 0.15F;
-                    double d1 = (levelIn.random.nextFloat() * 0.7F) + (double) 0.060000002F + 0.6D;
-                    double d2 = (double) (levelIn.random.nextFloat() * 0.7F) + (double) 0.15F;
-                    ItemStack itemstack1 = new ItemStack(blockState.getBlock());
-                    ItemEntity itementity = new ItemEntity(levelIn, (double) pos.getX() + d0, (double) pos.getY() + d1, (double) pos.getZ() + d2, itemstack1);
-                    itementity.setDefaultPickUpDelay();
-                    levelIn.addFreshEntity(itementity);
-                    frameBlockEntity.clear();
-                }
-            }
-        }
-    }
-
-    public void insertBlock(Level levelIn, BlockPos pos, BlockState state, BlockState handBlock) {
-        BlockEntity tileentity = levelIn.getBlockEntity(pos);
-        if (tileentity instanceof FrameBlockTile) {
-            FrameBlockTile frameBlockEntity = (FrameBlockTile) tileentity;
-            frameBlockEntity.clear();
-            frameBlockEntity.setMimic(handBlock);
-            levelIn.setBlock(pos, state.setValue(CONTAINS_BLOCK, Boolean.TRUE), 2);
-        }
     }
 
     @Override
@@ -149,10 +73,7 @@ public class StairsFrameBlock extends StairBlock implements EntityBlock {
 
     @Override
     public int getLightEmission(BlockState state, BlockGetter level, BlockPos pos) {
-        if (state.getValue(LIGHT_LEVEL) > 15) {
-            return 15;
-        }
-        return state.getValue(LIGHT_LEVEL);
+        return IFrameBlock.getLightEmission(state);
     }
 }
 //========SOLI DEO GLORIA========//
