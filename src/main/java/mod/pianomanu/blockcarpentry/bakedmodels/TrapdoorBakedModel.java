28720864be82d57b4681c5faeefe09957e7f650d
Manuel <jirsakm@web.de>
2022-11-14 20:07:45 +0100

    Fixed model for trapdoors, removed handle

--- src/main/java/mod/pianomanu/blockcarpentry/bakedmodels/TrapdoorBakedModel.java
+++ src/main/java/mod/pianomanu/blockcarpentry/bakedmodels/TrapdoorBakedModel.java
@@ -39,7 +39,7 @@ import java.util.List;
  * See {@link mod.pianomanu.blockcarpentry.util.ModelHelper} for more information
  *
  * @author PianoManu
- * @version 1.2 11/12/22
+ * @version 1.3 11/14/22
  */
 public class TrapdoorBakedModel implements IDynamicBakedModel {
     public static final ResourceLocation TEXTURE = new ResourceLocation("minecraft", "block/oak_planks");
@@ -56,7 +56,6 @@ public class TrapdoorBakedModel implements IDynamicBakedModel {
         if (mimic != null) {
             ModelResourceLocation location = BlockModelShaper.stateToModelLocation(mimic);
             BakedModel model = Minecraft.getInstance().getModelManager().getModel(location);
-            //model.getBakedModel().getQuads(mimic, side, rand, extraData);
             //only if model (from block saved in tile entity) exists:
             return getMimicQuads(state, side, rand, extraData, model);
         }
@@ -101,7 +100,7 @@ public class TrapdoorBakedModel implements IDynamicBakedModel {
             int design = extraData.get(FrameBlockTile.DESIGN);
             int desTex = extraData.get(FrameBlockTile.DESIGN_TEXTURE);
 
-            if (design == 0 || design == 1) {
+            if (design == 0) {
                 if (dir == north && open) {
                     quads.addAll(ModelHelper.createCuboid(0f, 1f, 0f, 1f, 13 / 16f, 1f, texture, tintIndex));
                 } else if (dir == west && open) {
@@ -116,71 +115,7 @@ public class TrapdoorBakedModel implements IDynamicBakedModel {
                     quads.addAll(ModelHelper.createCuboid(0f, 1f, 13 / 16f, 1f, 0f, 1f, texture, tintIndex));
                 }
             }
-            if (design == 1) {
-                /*DIFFERENT DIRECTIONS = DIFFERENT QUADS
-                MATCHING PAIRS:
-                        rotationFlag == 0: WEST/UP/CLOSED EAST/UP/CLOSED WEST/DOWN/CLOSED EAST/DOWN/CLOSED
-                        rotationFlag == 1: NORTH/UP/CLOSED SOUTH/UP/CLOSED NORTH/DOWN/CLOSED SOUTH/DOWN/CLOSED
-                        rotationFlag == 2: NORTH/UP/OPEN NORTH/DOWN/OPEN SOUTH/UP/OPEN SOUTH/DOWN/OPEN
-                        rotationFlag == 3: EAST/UP/OPEN EAST/DOWN/OPEN WEST/UP/OPEN WEST/DOWN/OPEN
-                 */
-                //int rotationFlag = 0;
-                if (open && half == bottom) {
-                    if (dir == north) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, 13 / 16f, 14 / 16f, 12 / 16f, 17 / 16f, 2, desTex));
-                    }
-                    if (dir == east) {
-                        quads.addAll(HandleBakedModel.createHandle(-1 / 16f, 4 / 16f, 13 / 16f, 14 / 16f, 6 / 16f, 10 / 16f, 3, desTex));
-                    }
-                    if (dir == south) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, 13 / 16f, 14 / 16f, -1 / 16f, 4 / 16f, 2, desTex));
-                    }
-                    if (dir == west) {
-                        quads.addAll(HandleBakedModel.createHandle(12 / 16f, 17 / 16f, 13 / 16f, 14 / 16f, 6 / 16f, 10 / 16f, 3, desTex));
-                    }
-                } else if (open && half == top) {
-                    if (dir == north) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, 2 / 16f, 3 / 16f, 12 / 16f, 17 / 16f, 2, desTex));
-                    }
-                    if (dir == east) {
-                        quads.addAll(HandleBakedModel.createHandle(-1 / 16f, 4 / 16f, 2 / 16f, 3 / 16f, 6 / 16f, 10 / 16f, 3, desTex));
-                    }
-                    if (dir == south) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, 2 / 16f, 3 / 16f, -1 / 16f, 4 / 16f, 2, desTex));
-                    }
-                    if (dir == west) {
-                        quads.addAll(HandleBakedModel.createHandle(12 / 16f, 17 / 16f, 2 / 16f, 3 / 16f, 6 / 16f, 10 / 16f, 3, desTex));
-                    }
-                }
-                if (half == bottom && !open) {
-                    if (dir == north) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, -1 / 16f, 4 / 16f, 2 / 16f, 3 / 16f, 1, desTex));
-                    }
-                    if (dir == east) {
-                        quads.addAll(HandleBakedModel.createHandle(13 / 16f, 14 / 16f, -1 / 16f, 4 / 16f, 6 / 16f, 10 / 16f, 0, desTex));
-                    }
-                    if (dir == south) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, -1 / 16f, 4 / 16f, 13 / 16f, 14 / 16f, 1, desTex));
-                    }
-                    if (dir == west) {
-                        quads.addAll(HandleBakedModel.createHandle(2 / 16f, 3 / 16f, -1 / 16f, 4 / 16f, 6 / 16f, 10 / 16f, 0, desTex));
-                    }
-                } else if (half == top && !open) {
-                    if (dir == north) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, 12 / 16f, 17 / 16f, 2 / 16f, 3 / 16f, 1, desTex));
-                    }
-                    if (dir == west) {
-                        quads.addAll(HandleBakedModel.createHandle(2 / 16f, 3 / 16f, 12 / 16f, 17 / 16f, 6 / 16f, 10 / 16f, 0, desTex));
-                    }
-                    if (dir == south) {
-                        quads.addAll(HandleBakedModel.createHandle(6 / 16f, 10 / 16f, 12 / 16f, 17 / 16f, 13 / 16f, 14 / 16f, 1, desTex));
-                    }
-                    if (dir == east) {
-                        quads.addAll(HandleBakedModel.createHandle(13 / 16f, 14 / 16f, 12 / 16f, 17 / 16f, 6 / 16f, 10 / 16f, 0, desTex));
-                    }
-                }
-            }
-            if (design == 2 || design == 3 || design == 4) {
+            if (design == 1 || design == 2 || design == 3) {
                 if (dir == north && open) {
                     //quads.addAll(ModelHelper.createCuboid(0f, 1f, 0f, 1f, 13 / 16f, 1f, texture, tintIndex));
                     quads.addAll(ModelHelper.createCuboid(0f, 3 / 16f, 3 / 16f, 13 / 16f, 13 / 16f, 1f, texture, tintIndex));
@@ -224,7 +159,7 @@ public class TrapdoorBakedModel implements IDynamicBakedModel {
                     quads.addAll(ModelHelper.createCuboid(13 / 16f, 1f, 13 / 16f, 1f, 3 / 16f, 13 / 16f, texture, tintIndex));
                     quads.addAll(ModelHelper.createCuboid(3 / 16f, 13 / 16f, 14 / 16f, 15 / 16f, 3 / 16f, 13 / 16f, glass, -1));
                 }
-                if (design == 3) {
+                if (design == 2) {
                     if (dir == north && open) {
                         //quads.addAll(ModelHelper.createCuboid(0f, 1f, 0f, 1f, 13 / 16f, 1f, texture, tintIndex));
                         quads.addAll(ModelHelper.createCuboid(7 / 16f, 9 / 16f, 3 / 16f, 13 / 16f, 13 / 16f, 1f, texture, tintIndex));
@@ -251,7 +186,7 @@ public class TrapdoorBakedModel implements IDynamicBakedModel {
                         quads.addAll(ModelHelper.createCuboid(3 / 16f, 13 / 16f, 13 / 16f, 1f, 7 / 16f, 9 / 16f, texture, tintIndex));
                     }
                 }
-                if (design == 4) {
+                if (design == 3) {
                     if (dir == north && open) {
                         //quads.addAll(ModelHelper.createCuboid(0f, 1f, 0f, 1f, 13 / 16f, 1f, texture, tintIndex));
                         quads.addAll(ModelHelper.createCuboid(3 / 16f, 13 / 16f, 3 / 16f, 13 / 16f, 14 / 16f, 15 / 16f, texture, tintIndex));
@@ -340,6 +275,71 @@ public class TrapdoorBakedModel implements IDynamicBakedModel {
                     if (dir == east) {
                         quads.addAll(HandleBakedModel.createHandle(13 / 16f, 14 / 16f, 12 / 16f, 13 / 16f, 6 / 16f, 10 / 16f, 0, desTex));
                         quads.addAll(HandleBakedModel.createHandle(13 / 16f, 14 / 16f, 1f, 17 / 16f, 6 / 16f, 10 / 16f, 0, desTex));
+                    }
+                }
+            }
+            if (design == 4) {
+                if (!open) {
+                    int yOffset = half == bottom ? 0 : 13;
+                    boolean upVisible, downVisible, nVisible, eVisible, sVisible, wVisible;
+                    boolean xStripe, zStripe;
+                    for (int x = 0; x < 16; x++) {
+                        for (int y = 0; y < 3; y++) {
+                            for (int z = 0; z < 16; z++) {
+                                upVisible = y == 2;
+                                downVisible = y == 0;
+                                xStripe = x % 3 == 0;
+                                zStripe = z % 3 == 0;
+                                nVisible = (zStripe && !xStripe) || z == 0;
+                                sVisible = (zStripe && !xStripe) || z == 15;
+                                eVisible = (xStripe && !zStripe) || x == 15;
+                                wVisible = (xStripe && !zStripe) || x == 0;
+                                if (xStripe || zStripe)
+                                    quads.addAll(ModelHelper.createCuboid(x / 16f, (x + 1) / 16f, (y + yOffset) / 16f, (y + yOffset + 1) / 16f, z / 16f, (z + 1) / 16f, texture, tintIndex, nVisible, sVisible, eVisible, wVisible, upVisible, downVisible));
+                            }
+                        }
+                    }
+                } else {
+                    if (dir == west || dir == east) {
+                        int xOffset = dir == east ? 0 : 13;
+                        boolean upVisible, downVisible, nVisible, eVisible, sVisible, wVisible;
+                        boolean yStripe, zStripe;
+                        for (int x = 0; x < 3; x++) {
+                            for (int y = 0; y < 16; y++) {
+                                for (int z = 0; z < 16; z++) {
+                                    wVisible = x == 0;
+                                    eVisible = x == 2;
+                                    yStripe = y % 3 == 0;
+                                    zStripe = z % 3 == 0;
+                                    nVisible = (zStripe && !yStripe) || z == 0;
+                                    sVisible = (zStripe && !yStripe) || z == 15;
+                                    upVisible = (yStripe && !zStripe) || y == 15;
+                                    downVisible = (yStripe && !zStripe) || y == 0;
+                                    if (yStripe || zStripe)
+                                        quads.addAll(ModelHelper.createCuboid((x + xOffset) / 16f, (x + xOffset + 1) / 16f, y / 16f, (y + 1) / 16f, z / 16f, (z + 1) / 16f, texture, tintIndex, nVisible, sVisible, eVisible, wVisible, upVisible, downVisible));
+                                }
+                            }
+                        }
+                    } else {
+                        int zOffset = dir == south ? 0 : 13;
+                        boolean upVisible, downVisible, nVisible, eVisible, sVisible, wVisible;
+                        boolean yStripe, xStripe;
+                        for (int x = 0; x < 16; x++) {
+                            for (int y = 0; y < 16; y++) {
+                                for (int z = 0; z < 3; z++) {
+                                    nVisible = z == 0;
+                                    sVisible = z == 2;
+                                    yStripe = y % 3 == 0;
+                                    xStripe = x % 3 == 0;
+                                    wVisible = (xStripe && !yStripe) || x == 0;
+                                    eVisible = (xStripe && !yStripe) || x == 15;
+                                    upVisible = (yStripe && !xStripe) || y == 15;
+                                    downVisible = (yStripe && !xStripe) || y == 0;
+                                    if (yStripe || xStripe)
+                                        quads.addAll(ModelHelper.createCuboid(x / 16f, (x + 1) / 16f, y / 16f, (y + 1) / 16f, (z + zOffset) / 16f, (z + zOffset + 1) / 16f, texture, tintIndex, nVisible, sVisible, eVisible, wVisible, upVisible, downVisible));
+                                }
+                            }
+                        }
                     }
                 }
             }
