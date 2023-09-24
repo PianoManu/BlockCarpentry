75d60d236d7b7d0645dead94f2a3c7c3f7160af6
Manuel <jirsakm@web.de>
2023-09-24 18:37:37 +0200

    Added description to the BlockEntityDebugItem

--- src/main/java/mod/pianomanu/blockcarpentry/item/BlockEntityDebugItem.java
+++ src/main/java/mod/pianomanu/blockcarpentry/item/BlockEntityDebugItem.java
@@ -1,11 +1,16 @@
 package mod.pianomanu.blockcarpentry.item;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -32,6 +37,23 @@ public class BlockEntityDebugItem extends BCToolItem {
             return InteractionResult.CONSUME;
         }
         return InteractionResult.FAIL;
+    }
+
+    /**
+     * This method adds a tooltip to the item shown when hovering over the item in an inventory
+     *
+     * @param itemStack the itemStack where the text shall be appended
+     * @param level     the current level
+     * @param component list of text components; for this item, we append either the item description or the reference to press shift
+     * @param flag      I don't really know what this does, I did not need it
+     */
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> component, TooltipFlag flag) {
+        if (Screen.hasShiftDown()) {
+            component.add(Component.translatable("tooltip.blockcarpentry.debug_item"));
+        } else {
+            component.add(Component.translatable("tooltip.blockcarpentry.shift"));
+        }
     }
 }
 //========SOLI DEO GLORIA========//
