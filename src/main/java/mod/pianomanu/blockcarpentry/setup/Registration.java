6841330a55473e7367d4750013c3e99e762ed0bf
Manuel <jirsakm@web.de>
2023-10-02 22:01:46 +0200

    Fixed Registration

--- src/main/java/mod/pianomanu/blockcarpentry/setup/Registration.java
+++ src/main/java/mod/pianomanu/blockcarpentry/setup/Registration.java
@@ -10,7 +10,9 @@ import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.PressurePlateBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -28,7 +30,7 @@ import org.apache.logging.log4j.Logger;
  * Just a normal registering class. See Forge-Documentation on how to register objects
  *
  * @author PianoManu
- * @version 1.1 05/31/22
+ * @version 1.4 10/02/23
  */
 @Mod.EventBusSubscriber(modid = BlockCarpentryMain.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 @SuppressWarnings("all") //only warning: datafixer for build()-method is null, but method is annotated as "NotNull"
@@ -45,9 +47,26 @@ public class Registration {
 
     public static final RegistryObject<FrameBlock> FRAMEBLOCK = BLOCKS.register("frameblock", () -> new FrameBlock(Block.Properties.of(Material.WOOD)
             .sound(SoundType.WOOD)
-            .strength(0.4f)
+            .strength(Blocks.OAK_PLANKS.getExplosionResistance())
+            .friction(Blocks.OAK_PLANKS.getFriction())
             .noOcclusion()));
     public static final RegistryObject<Item> FRAMEBLOCK_ITEM = ITEMS.register("frameblock", () -> new BaseFrameItem(FRAMEBLOCK.get(), new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY)));
+
+    public static void init() {
+        LOGGER.info("Registering blocks copy BlockCarpentry");
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        LOGGER.info("Registering items copy BlockCarpentry");
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        LOGGER.info("Registering tiles copy BlockCarpentry");
+        TILES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        LOGGER.info("Registering containers copy BlockCarpentry");
+        CONTAINERS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        //ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        //DIMENSIONS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        LOGGER.info("Registering points of interest copy Blockcarpentry");
+        POI_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
     public static final RegistryObject<BlockEntityType<FrameBlockTile>> FRAMEBLOCK_TILE = TILES.register("frameblock", () -> BlockEntityType.Builder.of(FrameBlockTile::new, FRAMEBLOCK.get()).build(null));
 
     public static final RegistryObject<FrameBlock> ILLUSION_BLOCK = BLOCKS.register("illusion_block", () -> new FrameBlock(Block.Properties.copy(FRAMEBLOCK.get())));
@@ -201,11 +220,15 @@ public class Registration {
     public static final RegistryObject<BlockEntityType<FrameBlockTile>> LAYERED_ILLUSION_TILE = TILES.register("illusion_layered_block", () -> BlockEntityType.Builder.of(FrameBlockTile::new, LAYERED_ILLUSIONBLOCK.get()).build(null));
 
 
-    //disabled signs - TODO fix signs
-    //public static final RegistryObject<StandingSignFrameBlock> STANDING_SIGN_FRAMEBLOCK = BLOCKS.register("standing_frame_sign", () -> new StandingSignFrameBlock(Block.Properties.copy(FRAMEBLOCK.get())));
-    //public static final RegistryObject<WallSignFrameBlock> WALL_SIGN_FRAMEBLOCK = BLOCKS.register("wall_frame_sign", () -> new WallSignFrameBlock(Block.Properties.copy(FRAMEBLOCK.get())));
-    //public static final RegistryObject<Item> SIGN_FRAME_ITEM = ITEMS.register("frame_sign", () -> new FrameSignItem((new Item.Properties()).maxStackSize(16).tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY), Registration.STANDING_SIGN_FRAMEBLOCK.get(), Registration.WALL_SIGN_FRAMEBLOCK.get()));
-    //public static final RegistryObject<BlockEntityType<SignFrameTile>> SIGN_FRAME_TILE = TILES.register("frame_sign", () -> BlockEntityType.Builder.of(SignFrameTile::new, STANDING_SIGN_FRAMEBLOCK.get(), WALL_SIGN_FRAMEBLOCK.get()).build(null));
+    public static final RegistryObject<StandingSignFrameBlock> STANDING_SIGN_FRAMEBLOCK = BLOCKS.register("standing_frame_sign", () -> new StandingSignFrameBlock(Block.Properties.copy(FRAMEBLOCK.get()).noCollission()));
+    public static final RegistryObject<WallSignFrameBlock> WALL_SIGN_FRAMEBLOCK = BLOCKS.register("wall_frame_sign", () -> new WallSignFrameBlock(Block.Properties.copy(FRAMEBLOCK.get())));
+    public static final RegistryObject<Item> SIGN_FRAME_ITEM = ITEMS.register("frame_sign", () -> new FrameSignItem((new Item.Properties()).stacksTo(16).tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY), Registration.STANDING_SIGN_FRAMEBLOCK.get(), Registration.WALL_SIGN_FRAMEBLOCK.get()));
+    public static final RegistryObject<BlockEntityType<SignFrameTile>> SIGN_FRAME_TILE = TILES.register("frame_sign", () -> BlockEntityType.Builder.of(SignFrameTile::new, STANDING_SIGN_FRAMEBLOCK.get(), WALL_SIGN_FRAMEBLOCK.get()).build(null));
+
+    public static final RegistryObject<StandingSignFrameBlock> STANDING_SIGN_ILLUSIONBLOCK = BLOCKS.register("standing_illusion_sign", () -> new StandingSignFrameBlock(Block.Properties.copy(FRAMEBLOCK.get()).noCollission()));
+    public static final RegistryObject<WallSignFrameBlock> WALL_SIGN_ILLUSION_BLOCK = BLOCKS.register("wall_illusion_sign", () -> new WallSignFrameBlock(Block.Properties.copy(FRAMEBLOCK.get())));
+    public static final RegistryObject<Item> SIGN_ILLUSION_ITEM = ITEMS.register("illusion_sign", () -> new FrameSignItem((new Item.Properties()).stacksTo(16).tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY), Registration.STANDING_SIGN_ILLUSIONBLOCK.get(), Registration.WALL_SIGN_ILLUSION_BLOCK.get()));
+    public static final RegistryObject<BlockEntityType<SignFrameTile>> SIGN_ILLUSION_TILE = TILES.register("illusion_sign", () -> BlockEntityType.Builder.of(SignFrameTile::new, STANDING_SIGN_ILLUSIONBLOCK.get(), WALL_SIGN_ILLUSION_BLOCK.get()).build(null));
 
     public static final RegistryObject<SlopeFrameBlock> SLOPE_FRAMEBLOCK = BLOCKS.register("frame_slope", () -> new SlopeFrameBlock(() -> FRAMEBLOCK.get().defaultBlockState(), Block.Properties.copy(FRAMEBLOCK.get())));
     public static final RegistryObject<Item> SLOPE_FRAME_ITEM = ITEMS.register("frame_slope", () -> new BaseFrameItem(SLOPE_FRAMEBLOCK.get(), new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY)));
@@ -217,26 +240,13 @@ public class Registration {
     public static final RegistryObject<BlockEntityType<FrameBlockTile>> EDGED_SLOPE_FRAME_TILE = TILES.register("frame_edged_slope", () -> BlockEntityType.Builder.of(FrameBlockTile::new, EDGED_SLOPE_FRAMEBLOCK.get()).build(null));
 
 
-    public static void init() {
-        LOGGER.info("Registering blocks copy BlockCarpentry");
-        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        LOGGER.info("Registering items copy BlockCarpentry");
-        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        LOGGER.info("Registering tiles copy BlockCarpentry");
-        TILES.register(FMLJavaModLoadingContext.get().getModEventBus());
-        LOGGER.info("Registering containers copy BlockCarpentry");
-        CONTAINERS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        //ENTITIES.register(FMLJavaModLoadingContext.get().getModEventBus());
-        //DIMENSIONS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        LOGGER.info("Registering points of interest copy Blockcarpentry");
-        POI_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-
-
     public static final RegistryObject<HammerItem> HAMMER = ITEMS.register("hammer", () -> new HammerItem(new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY).stacksTo(1)));
     public static final RegistryObject<TextureWrenchItem> TEXTURE_WRENCH = ITEMS.register("texture_wrench", () -> new TextureWrenchItem(new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY).stacksTo(1)));
     public static final RegistryObject<ChiselItem> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY).stacksTo(1)));
     public static final RegistryObject<PaintbrushItem> PAINTBRUSH = ITEMS.register("paintbrush", () -> new PaintbrushItem(new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY).stacksTo(1)));
+
+    public static final RegistryObject<BlockEntityDebugItem> DEBUG_ITEM = ITEMS.register("block_entity_debug_item", () -> new BlockEntityDebugItem(new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY).stacksTo(1).rarity(Rarity.EPIC)));
+    public static final RegistryObject<Item> EXPLOSION_RESISTANCE_BALL = ITEMS.register("explosion_resistance_ball", () -> new Item(new Item.Properties().tab(BlockCarpentryMain.BlockCarpentryItemGroup.BLOCK_CARPENTRY).rarity(Rarity.UNCOMMON)));
 
 }
 //========SOLI DEO GLORIA========//
